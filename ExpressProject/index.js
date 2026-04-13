ac4a5ccc0bbcae9f6863b7bae622bf2fa07c4617
Mvinit2245 <vinitmorankar22@gmail.com>
2026-04-13 22:47:08 +0530

    API Learning post,get

--- ExpressProject/index.js
+++ ExpressProject/index.js
@@ -1,6 +1,7 @@
 let express = require("express")
 
 let app = express()
+app.use(express.json())
 
 app.get("/", (req,res) =>{ //http://localhost:8000/
     res.send({status:1,msg:"Home Page API"})
@@ -12,6 +13,11 @@ app.get("/news", (req,res) =>{
 
 app.get("/products", (req,res) =>{
     res.send({status:1,msg:"Products API"})
+})
+
+app.post("/login", (req,res) =>{
+    console.log(req.body)//object
+    res.send({status:1,msg:"Login API", data:req.body})
 })
 
 app.listen("8000")

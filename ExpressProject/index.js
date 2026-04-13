8f7e9e86e876562fcec09f509671feeebf0d5737
Mvinit2245 <vinitmorankar22@gmail.com>
2026-04-13 22:08:10 +0530

    API Learning

--- ExpressProject/index.js
+++ ExpressProject/index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+let express = require("express")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+let app = express()
+
+app.get("/", (req,res) =>{ //http://localhost:8000/
+    res.send({status:1,msg:"Home Page API"})
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get("/news", (req,res) =>{
+    res.send({status:1,msg:"News API"})
 })
+
+app.get("/products", (req,res) =>{
+    res.send({status:1,msg:"Products API"})
+})
+
+app.listen("8000")
